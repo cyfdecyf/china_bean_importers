0df97b3d48ae5076bc631c53a933a5fcc219ad0c
Chen Yufei <cyfdecyf@gmail.com>
2026-09-12 12:44:31 +0800

    Import beangulp subpackage importers from package init.
    Also install beangulp in CI for the smoke test.

--- china_bean_importers/__init__.py
+++ china_bean_importers/__init__.py
@@ -1,33 +1,19 @@
-from china_bean_importers import (
+# ruff: disable[F401]
+from china_bean_importers.beangulp import (
     alipay_mobile,
     alipay_web,
     boc_credit_card,
     boc_debit_card,
-    ccb_debit_card,
     ccb_credit_card,
+    ccb_debit_card,
     cmb_debit_card,
     cmbc_credit_card,
     cmbc_debit_card,
+    hsbc_hk,
     icbc_credit_card,
     icbc_debit_card,
-    hsbc_hk,
+    spdb_debit_card,
     thu_ecard_old,
     wechat,
 )
-
-__all__ = [
-    "alipay_mobile",
-    "alipay_web",
-    "boc_credit_card",
-    "boc_debit_card",
-    "ccb_debit_card",
-    "ccb_credit_card",
-    "cmb_debit_card",
-    "cmbc_credit_card",
-    "cmbc_debit_card",
-    "icbc_credit_card",
-    "icbc_debit_card",
-    "hsbc_hk",
-    "thu_ecard_old",
-    "wechat",
-]
+# ruff: enable[F401]
